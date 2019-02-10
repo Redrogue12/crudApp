f8b960fd547f911d8989c432014dd865024db214
Redrogue12 <exgc12@gmail.com>
2019-02-10 02:09:42 -0400

    moved createError

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const createError = require('http-errors')
 
 require('dotenv').config()
 
--- routes/articles.js
+++ routes/articles.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors')
 
 // Bring in Models
 let Article = require('../models/article')
