93a18293ce0b7183561e9def2a18c48a550ae2b6
Redrogue12 <exgc12@gmail.com>
2019-02-11 18:45:03 -0400

    test

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('dotenv').config()
 const articles = require('./routes/articles.js')
 
 // Set Port
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 // Init App
 const app = express()
@@ -42,20 +42,20 @@ mongoose
   .catch(err => console.log(err));
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 // Start Server
 app.listen(PORT, () => {
